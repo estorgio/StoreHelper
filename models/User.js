a13eb50f14dde3c4cf1d2b04db0228cbd6ca7b51
estorgio <fortunato.estorgio@gmail.com>
2018-02-28 19:17:33 +0800

    Add aditional fields for Users schema

--- models/User.js
+++ models/User.js
@@ -6,7 +6,16 @@ var userSchema = new mongoose.Schema({
   password: String,
   firstname: String,
   lastname: String,
+
+  gender: {type: String, default: null},
+  birthdate: {type: Date, default: null},
+  civil_status: {type: String, default: null},
+  address: {type: String, default: null},
+  phone: {type: String, default: null},
+
   type: {type: String, default: 'cashier'},
+  branch: {type: String, default: null},
+
   created: {type: Date, default: Date.now},
   last_login: {type: Date, default: Date.now},
   enabled: {type: Boolean, default: true},
